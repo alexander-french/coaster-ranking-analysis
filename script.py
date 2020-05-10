07630e79571f85d61c9870cfc2fbb1e5b0fb3f11
Alexander French <alexander.french.uk@gmail.com>
2020-05-10 17:37:33 +0100

    added func which shows top n coaster by year

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 wood = pd.read_csv('Golden_Ticket_Award_Winners_Wood.csv')
 steel = pd.read_csv('Golden_Ticket_Award_Winners_Steel.csv')
 
-#function to plot rankings over time for 1 roller coaster here:
+#function to plot rankings over time for 1 roller coaster:
 def rank_over_time(data, coaster, park):
     ranking_df = data[(data["Name"] == coaster) & (data["Park"] == park)]
     year = ranking_df["Year of Rank"]
@@ -25,7 +25,7 @@ def rank_over_time(data, coaster, park):
 rank_over_time(wood, "El Toro", "Six Flags Great Adventure")
 plt.clf()
 
-# write function to plot rankings over time for 2 roller coasters here:
+#function to plot rankings over time for 2 roller coasters:
 def rank_comparison(data, coaster_1, park_1, coaster_2, park_2):
     df_1 = data[(data["Name"] == coaster_1) & (data["Park"] == park_1)]
     df_2 = data[(data["Name"] == coaster_2) & (data["Park"] == park_2)]
@@ -44,11 +44,21 @@ rank_comparison(wood, "El Toro", "Six Flags Great Adventure", "Boulder Dash", "L
 plt.clf()
 
 # write function to plot top n rankings over time here:
+def top_n(data, n):
+    df = data[data["Rank"] <= n]
+    ax = plt.subplot()
+    ax.invert_yaxis()
+    plt.xlabel("Year")
+    plt.ylabel("Golden Ticket Ranking")
+    plt.title("Top " + str(n) + " coasters each year")
+    for coaster in set(df["Name"]):
+        ranking = df[df["Name"] == coaster]
+        ax.plot(ranking["Year of Rank"], ranking["Rank"], marker='o', label=coaster)
+    plt.legend()
+    plt.show()
 
-
-
-
-
+#test case
+top_n(wood, 5)
 
 
 
